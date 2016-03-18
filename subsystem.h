77757a4f4578e016b01184fd365948b48af33adb
suvorovroman <suvorovroman@gmail.com>
2016-03-18 16:38:04 +0400

    Update subsystem.h

--- subsystem.h
+++ subsystem.h
@@ -14,12 +14,23 @@ private:
     /** \brief Таблица методов объекта (интерфейс объекта). */
     typedef std::unsorted_map<wxString, method> iface;
     
-    short send(const wxString& _m, argument& _a)
+    /** \brief Вызов метода объекта с параметрами.
+     \param[in]   _m  Имя метода.
+     \param[in]   _a  Список аргументов.
+     \param[out]  _v  Возвращаемое значение.
+     \return Возвращаемое значение.
+     \exception failure Неизвестный матод объекта.
+    */
+    short send(const wxString& _m, argument& _a, double& _v)
     {
       iface& i = get_iface();
       iface::iterator it = i.find(_m);
       if(it != i.end())
+        return (this->*(it->second))(_a, _v);
+      else
       {
+        wxLogError(wxT("Неизвестный метод \"%s\" объекта"), _m);
+        throw failure();
       }
     }
     
