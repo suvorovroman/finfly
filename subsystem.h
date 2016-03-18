10ca80bcde062c7098dbf18c59a7ece542cbe89f
suvorovroman <suvorovroman@gmail.com>
2016-03-18 16:20:35 +0400

    Update subsystem.h

--- subsystem.h
+++ subsystem.h
@@ -14,6 +14,15 @@ private:
     /** \brief Таблица методов объекта (интерфейс объекта). */
     typedef std::unsorted_map<wxString, method> iface;
     
+    short send(const wxString& _m, argument& _a)
+    {
+      iface& i = get_iface();
+      iface::iterator it = i.find(_m);
+      if(it != i.end())
+      {
+      }
+    }
+    
   protected:
     
     /** \brief Получить таблицу методов объекта для производного класса.
