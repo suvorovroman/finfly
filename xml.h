10f1a8705ab220087bc640ccad98d66ff953a113
suvorovroman <suvorovroman@gmail.com>
2016-07-01 13:51:45 +0400

    Update xml.h

--- xml.h
+++ xml.h
@@ -1,3 +1,22 @@
 namespace xml
 {
+  
+  class handler:public HandlerBase
+  {
+    
+  };
+  
+  class internal_handler:public handler
+  {
+    
+  };
+  
+  class external_handler:public handler
+  {
+  private:
+  
+    lua_state State;
+    
+  };
+  
 }
