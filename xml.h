0d44d05a724043c64aa267d37a0a2cac33479e25
suvorovroman <suvorovroman@gmail.com>
2016-07-12 18:23:43 +0400

    Update xml.h

--- xml.h
+++ xml.h
@@ -1,21 +1,29 @@
 namespace xml
 {
   
-  class handler:public HandlerBase
+  class interpretator:public HandlerBase, public XMLFormatTarget
   {
-    
-  };
+  public:
   
-  class internal_handler:public handler
-  {
+    interpretator(const wxString& _f)
+    {
+      L = luaL_newstate();
+      if(L)
+      {
+        lua_atpanic(L, cf_panic);
+        if(luaL_loadfile(L, _f))
+        {
+          wxLogError("%s", lua_tostring(L, -1));
+          throw failure();
+        }
+      }
+      else
+        throw std::bad_alloc("failed to allocate new Lua state for xml handler");
+    }
     
-  };
-  
-  class external_handler:public handler
-  {
   private:
   
-    lua_state State;
+    lua_State* L;
     
   };
   
