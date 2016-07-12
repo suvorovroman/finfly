49d15b8f2529bcdb4682dbcf57f24be9f9e4d68e
suvorovroman <suvorovroman@gmail.com>
2016-07-12 19:07:10 +0400

    Update xml.h

--- xml.h
+++ xml.h
@@ -36,6 +36,20 @@ namespace xml
       return 0;
     }
     
+    void startDocument()
+    {
+      luaL_checkstack(L, 4);
+      /* chunk */
+      if(lua_pcall(L, 0, 1, 0))
+        throw SAXException(lua_tostring(L, -1));
+      /* handler */
+      lua_pushcfunction(L, cf_startDocument); 
+      lua_pushvalua(L, -2);
+      if(lua_pcall(L, 1, 3, 0))
+        throw SAXException(lua_tostring(L, -1));
+      /* element.finish element.start document.finish handler */
+    }
+    
   };
   
 }
